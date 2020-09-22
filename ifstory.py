621d38dd4a327e5b980260aa71b0d37456e6c60a
Mission Bit <missionbit@Missions-MacBook-Pro.local>
2020-09-21 18:14:24 -0700

    conditional for ifstory.py

---
+++ ifstory.py
@@ -0,0 +1,11 @@
+x=input("yes or no")
+if x=="yes":
+    y=input("yes or no")
+    if x=="yes":
+        print ("a")
+    if x=="no":
+        print ("b")
+if x=="no":
+    z=input("yes or no")
+    if z=="yes":
+        print("c")
