35a8fca3430360738941e3ee91b11651a5e8e770
Mission Bit <missionbit@Missions-MacBook-Pro.local>
2020-09-23 17:29:49 -0700

    Gradebook.py

---
+++ gradebook.py
@@ -0,0 +1,12 @@
+grade = int(input("Grade (1-100):"))
+
+if grade >= 90:
+    print("U get A")
+elif grade >= 80:
+    print("U get B")
+elif grade >= 70:
+    print("U get C")
+elif grade >= 60:
+    print("U get D")
+elif grade < 60:
+    print("U dum")
