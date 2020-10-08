d9e4fcd93fa3c3aede2f6a0b041c0216514eae55
Mission Bit <missionbit@Missions-MacBook-Pro.local>
2020-10-07 18:28:26 -0700

    score system

--- rockps2.py
+++ rockps2.py
@@ -1,4 +1,6 @@
 import random
+cp_score = 0
+your_score = 0
 def opponent():
     #1 = compost 2=recycle 3=trash
     x = random.randint(1,3)
@@ -21,37 +23,61 @@ def game_code(p1,p2):
         print("draw")
     elif p1 == 1 and p2 == 2:
         print("You Lose")
+        return "a"
     elif p1 == 2 and p2 == 3:
         print("You Lose")
+        return "a"
     elif p1 == 3 and p2 == 1:
         print("You Lose")
+        return "a"
     elif p1 == 1 and p2 == 3:
         print("You Win")
+        return "b"
     elif p1 == 2 and p2 == 1:
         print("You Win")
+        return "b"
     elif p1 == 3 and p2 == 2:
         print("You Win")
+        return "b"
     else:
         print("Please choose a number")
-    
-def streaks():
+        return "c"
+        
 
-def continuing():
+def scores(fx):
+    if fx == "a":
+        global cp_score
+        cp_score = cp_score + 1
+        return cp_score
+    if fx == "b":
+        global your_score
+        your_score = your_score + 1
+        return your_score
+    if fx == "c": 
+        print("Invalid Choice")
+        return
+
+def continuing(wins):
     continuee = int(input("Do you want to play again? 1. Yes 2. No"))
     if continuee == 1:
         return True
     else:
+        print("Thanks for playing!")
+        print(wins)
         return False
 
-    
 def main():
     Play = True
-    your_score = 0
-    cp_score = 0
+    rounds = 0
+    your_wins = 0
     while Play == True:
+        rounds = rounds+1
+        print(rounds)
         CP = int(opponent())
         you = int(your_choice())
         CP_choice(CP)
-        game_code(CP,you)
-        Play = continuing()
+        winner = game_code(CP,you)
+        scores(winner)
+        your_wins = your_score/rounds
+        Play = continuing(your_wins)
 main()
